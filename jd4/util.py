7af739102b9fb13d57c2e07fa6a08fe5b69186da
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-12-15 23:33:21 -0600

    feat: adding tarfile member sanitization to extractall() (#10)

--- jd4/util.py
+++ jd4/util.py
@@ -101,7 +101,29 @@ def chmod_recursive(_dir, mode):
 
 def extract_tar_file(file_path, dest_dir):
     with tarfile.open(file_path) as file:
-        file.extractall(path=dest_dir)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(file, path=dest_dir)
 
 
 def extract_zip_file(file_path, dest_dir):
